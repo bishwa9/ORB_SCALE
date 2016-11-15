91b1f2f55267b05a0d610e5a8aff497ae5223add
bishwa9 <sanghamitra@SanghamitrasMBP.wv.cc.cmu.edu>
2016-11-15 16:14:47 -0500

    Tushar Check

--- fundamental/fundamentalTest.cpp
+++ fundamental/fundamentalTest.cpp
@@ -88,18 +88,91 @@ int main( int argc, char** argv )
 
   Mat K = (Mat_<double>(3,3) << 800, 0, 320, 0, 800, 240, 0, 0, 1);
 
-  Mat H = findHomography( obj, scene, CV_RANSAC );
-  Mat T2 = K.inv() * H.inv() * K;
+  Mat F = findFundamentalMat(obj, scene);
 
-  std::cout << T2 << std::endl;
+  Mat w;
+  SVD::compute(F, w);
 
+  std::cout << "F\n" << F << std::endl;
+  std::cout << w << std::endl;
+
+  Mat E = K.t() * F * K;
+  Mat u, vt;
+  SVD::compute(E, w, u, vt);
+
+  std::cout << "BEFORE:\n";
+  std::cout << "E\n" << E << std::endl;
+  std::cout << "u\n" << u << std::endl;
+  std::cout << "w\n" << w << std::endl;
+  std::cout << "vt\n" << vt << std::endl;
+
+  w.at<double>(0,0) = ( w.at<double>(0,0) + w.at<double>(1,0) ) / 2.0;
+  w.at<double>(1,0) = w.at<double>(0,0);
+  w.at<double>(2,0) = 0.0;
+  Mat w_ = (Mat_<double>(3,3) << w.at<double>(0,0), 0, 0, 0, w.at<double>(1,0), 0, 0, 0, w.at<double>(2,0));
+
+  std::cout << "BEFORE_2:\n";
+  std::cout << "E\n" << E << std::endl;
+  std::cout << "u\n" << u << std::endl;
+  std::cout << "w\n" << w_ << std::endl;
+  std::cout << "vt\n" << vt << std::endl;
+
+  E = u * w_ * vt;
+  SVD::compute(E, w, u, vt);
+  w_ = (Mat_<double>(3,3) << w.at<double>(0,0), 0, 0, 0, w.at<double>(1,0), 0, 0, 0, w.at<double>(2,0));
+  std::cout << "AFTER:\n";
+  std::cout << "E\n" << E << std::endl;
+  std::cout << "u\n" << u << std::endl;
+  std::cout << "w\n" << w_ << std::endl;
+  std::cout << "vt\n" << vt << std::endl;
+
+  //extract all four possible translations and rotations
+  vector<Mat> p_r, p_t;
+  p_r.push_back(u*w_*vt);     p_t.push_back(u.col(2));
+  p_r.push_back(u*w_*vt);     p_t.push_back(u.col(2));
+  p_r.push_back(u*w_.t()*vt); p_t.push_back(-1*u.col(2));
+  p_r.push_back(u*w_.t()*vt); p_t.push_back(-1*u.col(2));
+
+  for(int i = 0; i < 4; i++)
+  {
+    Mat t1 = (Mat_<double>(3,1) << 0, 0, 0); Mat R1 = Mat::eye(3, 3, CV_64F);
+    Mat t2 = p_t[i]; Mat R2 = p_r[i];
+    Mat T1, T2;
+    hconcat(R1, t1, T1); 
+    hconcat(R2, t2, T2);
+    Mat M1 = K*T1;
+    Mat M2 = K*T2;
+    cv::Mat pt(1,4,CV_64FC4);
+    cv::Mat cam0pts(1,3,CV_64FC2);
+    cam0pts.at<float>(0,0) = obj[0].x;
+    cam0pts.at<float>(0,1) = obj[0].y;
+    cam0pts.at<float>(0,2) = 1;
+    cv::Mat cam1pts(1,3,CV_64FC2);
+    cam1pts.at<float>(0,0) = scene[0].x;
+    cam1pts.at<float>(0,1) = scene[0].y;
+    cam1pts.at<float>(0,2) = 1;
+    triangulatePoints(M1, M2, cam0pts, cam1pts, pt);
+
+    std::cout << "R1\n" << R1 << "\nR2\n" << R2 << std::endl; 
+
+    std::cout << "X_1: " << pt.at<float>(0,0)/pt.at<float>(0,3) << ", ";
+    std::cout << "Y_1: " << pt.at<float>(0,1)/pt.at<float>(0,3) << ", ";
+    std::cout << "Z_1: " << pt.at<float>(0,2)/pt.at<float>(0,3) << ", ";
+    std::cout << "1_1: " << pt.at<float>(0,3)/pt.at<float>(0,3) << std::endl;
+
+    Mat pt_2 = T2 * pt;
+    std::cout << "X_2: " << pt_2.at<float>(0,0)/pt_2.at<float>(0,3) << ", ";
+    std::cout << "Y_2: " << pt_2.at<float>(0,1)/pt_2.at<float>(0,3) << ", ";
+    std::cout << "Z_2: " << pt_2.at<float>(0,2)/pt_2.at<float>(0,3) << ", ";
+    std::cout << "1_2: " << pt_2.at<float>(0,3)/pt_2.at<float>(0,3) << std::endl << std::endl;
+  }
   //-- Get the corners from the image_1 ( the object to be "detected" )
   std::vector<Point2f> obj_corners(4);
   obj_corners[0] = Point(0,0); obj_corners[1] = Point( img_object.cols, 0 );
   obj_corners[2] = Point( img_object.cols, img_object.rows ); obj_corners[3] = Point( 0, img_object.rows );
   std::vector<Point2f> scene_corners(4);
 
-  perspectiveTransform( obj_corners, scene_corners, H);
+  perspectiveTransform( obj_corners, scene_corners, F );
 
 
   //-- Draw lines between the corners (the mapped object in the scene - image_2 )
